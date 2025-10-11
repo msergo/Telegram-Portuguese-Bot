c3113377f63c1afaca3721ac8569b3b40e65a6fd
Sergo <localost>
2025-10-11 23:55:30 +0300

    feat: Add migration files for users and cached articles tables

--- src/migration.rs
+++ src/migration.rs
@@ -6,11 +6,11 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20250101_000001_create_users_table::Migration),
-            Box::new(m20250101_000002_create_cached_articles_table::Migration),
+            Box::new(m20251011_000001_create_users_table::Migration),
+            Box::new(m20251011_000002_create_cached_articles_table::Migration),
         ]
     }
 }
 
-pub mod m20250101_000001_create_users_table;
-pub mod m20250101_000002_create_cached_articles_table;
+pub mod m20251011_000001_create_users_table;
+pub mod m20251011_000002_create_cached_articles_table;

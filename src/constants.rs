57414d2964f371a6af6fac658c429ac74837e3e8
sergo <localhost>
2025-10-15 22:34:19 +0300

    feat: Add language direction constants

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+// Language direction constants
+pub const LANG_PT_EN: &str = "pten";
+pub const LANG_EN_PT: &str = "enpt";
+pub const LANG_IT_EN: &str = "iten";
+pub const LANG_EN_IT: &str = "enit";
+
+// Default language direction
+pub const DEFAULT_LANG_DIRECTION: &str = LANG_PT_EN;
